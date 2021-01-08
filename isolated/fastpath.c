72a3fcf163ba3828592b1bd7b66387cdca20d158
crazyqnt <crazyqnt@gmail.com>
2021-01-08 13:59:57 +0100

    Wrote first comparison between vectorized and scalar implementations

--- isolated/fastpath.c
+++ isolated/fastpath.c
@@ -42,7 +42,7 @@ struct configuration config;
 
 __m256i qman_set_vec(__m512i t, __m256i id, __m256i rate, __m256i avail,
     __m256i max_chunk, __m256i flags, __mmask8 k) {
-    printf("qman_set_vec\n");
+    //printf("qman_set_vec"); will get called plenty of times, but the mask is empty
     return _mm256_set1_epi32(0);
 }
 
@@ -132,33 +132,118 @@ static struct rte_mbuf *create_payload_package(uint32_t* cur_seq) {
     return tmb;
 }
 
-static void init_config() {
+static void init_systems() {
+    memset(&state_base, 0, sizeof(state_base));
+
     config.shm_len = 0xFFFFFFFFFFFFFFFF; // to prevent asserts, hopefully
 }
 
-int run_measurement() {
-    struct flextcp_pl_flowst *fs = &state_base.flowst[0];
+unsigned long long run_measurement(int vectorized) {
+    init_systems();
+    struct flextcp_pl_flowst *fss[8];
     struct dataplane_context ctx;
     memset(&ctx, 0, sizeof(ctx));
 
-    flow_init(0, 1024 * 8, 1024 * 8, 123456);
+    uint32_t seqs[8];
+    for (unsigned i = 0; i < 8; i++) {
+      fss[i] = &state_base.flowst[i];
+      flow_init(i, 1024 * 1024, 1024 * 1024, 123456);
+      seqs[i] = fss[i]->rx_next_seq;
+    }
 
-    uint32_t seq = fs->rx_next_seq;
-    struct rte_mbuf *tmb = create_payload_package(&seq);
+    struct rte_mbuf *mbufs[1024];
+    for (unsigned i = 0; i < 1024; i++) {
+      mbufs[i] = create_payload_package(&seqs[i % 8]);
+    }
 
-    struct tcp_opts topts;
+    unsigned long long start, end;
+    if (vectorized != 0) {
+      start = __rdtsc();
+      MEM_BARRIER();
+      for (unsigned i = 0; i < 1024; i += 8) {
+        struct tcp_opts tcpopts[8];
+        __m512i ctx_vec = _mm512_set1_epi64((uintptr_t) &ctx);
+        __m512i bhs_vec = _mm512_loadu_epi64(&mbufs[i]);
+        __m512i fss_vec = _mm512_set_epi64((uintptr_t) (fss + 7), (uintptr_t) (fss + 6),
+          (uintptr_t) (fss + 5), (uintptr_t) (fss + 4), (uintptr_t) (fss + 3),
+          (uintptr_t) (fss + 2), (uintptr_t) (fss + 1), (uintptr_t) (fss + 0)  
+        );
+        __m512i tcpopts_vec = _mm512_set_epi64((uintptr_t) (tcpopts + 7), (uintptr_t) (tcpopts + 6),
+          (uintptr_t) (tcpopts + 5), (uintptr_t) (tcpopts + 4), (uintptr_t) (tcpopts + 3),
+          (uintptr_t) (tcpopts + 2), (uintptr_t) (tcpopts + 1), (uintptr_t) (tcpopts + 0)  
+        );
 
-    fast_flows_packet_parse(&ctx, (struct network_buf_handle*)tmb, (void**)&fs, &topts);
+        __mmask8 mask = _cvtu32_mask8(0xFF);
 
-    printf("%lu\n", (uintptr_t)fs);
+        fast_flows_packet_parse_vec(ctx_vec, bhs_vec, fss_vec, tcpopts_vec, mask);
 
-    fast_flows_packet(&ctx, (struct network_buf_handle*)tmb, (void**)&fs, &topts, 0);
+        __m512i fss_loaded = _mm512_loadu_epi64(&fss[0]);
+        __mmask8 cmp = _mm512_cmpneq_epi64_mask(fss_loaded, _mm512_set1_epi64(0));
 
-    return 0;
+        __mmask8 if_mask = _kand_mask8(cmp, mask);
+        __m256i ts_vec = _mm256_set1_epi32(0);
+
+        fast_flows_packet_vec(ctx_vec, bhs_vec, fss_loaded, tcpopts_vec, ts_vec, if_mask);
+
+        if (_cvtmask8_u32(cmp) != 0xFF) {
+          fprintf(stderr, "fast_flows_packet_parse returned NULL, iteration (vectorized) %u\n", i);
+          abort();
+        }
+
+        // "flush" transmit buffer
+        assert(ctx.tx_num == 8);
+        ctx.tx_num = 0;
+      }
+      MEM_BARRIER();
+      end = __rdtsc();
+    } else {
+      start = __rdtsc();
+      MEM_BARRIER();
+      for (unsigned i = 0; i < 1024; i++) {
+        struct tcp_opts topts;
+        fast_flows_packet_parse(&ctx, (struct network_buf_handle*)mbufs[i], (void**)&fss[i % 8], &topts);
+
+        if (fss[i % 8] == NULL) {
+          fprintf(stderr, "fast_flows_packet_parse returned NULL, iteration %u\n", i);
+          abort();
+        }
+
+        fast_flows_packet(&ctx, (struct network_buf_handle*)mbufs[i], (void*)fss[i % 8], &topts, 0);
+        if (i % 8 == 7) {
+          // "flush" transmit buffer
+          assert(ctx.tx_num == 8);
+          ctx.tx_num = 0;
+        }
+      }
+      MEM_BARRIER();
+      end = __rdtsc();
+    }
+
+    for (unsigned i = 0; i < 1024; i++) {
+      free(mbufs[i]);
+    }
+
+    return end - start;
 }
 
 int main(int argc, char *argv[]) {
-    memset(&state_base, 0, sizeof(state_base));
+    printf("Warmup...\n");
+    for (unsigned i = 0; i < 8; i++) {
+      run_measurement((i % 2 == 0) ? 1 : 0);
+    }
 
-    return run_measurement();
+    printf("Go!\n");
+    unsigned long long vectorized = 0, scalar = 0;
+    for (unsigned i = 0; i < 64; i++) {
+      vectorized += run_measurement(1);
+    }
+    for (unsigned i = 0; i < 64; i++) {
+      scalar += run_measurement(0);
+    }
+
+    printf("Results:\n");
+    printf("Vectorized: %.1f cycles\n", ((double)vectorized) / 64.0);
+    printf("Scalar    : %.1f cycles\n", ((double)scalar) / 64.0);
+
+    return 0;
 }
